f2d976d031b97b59814047f32b8f1db14cb9ab88
Th.Manetas <manetas.th@gmail.com>
2024-12-10 20:47:26 +0200

    second commit

---
+++ src/gr/aueb/cf/ch1/InitialsApp.java
@@ -0,0 +1,15 @@
+package gr.aueb.cf.ch1;
+
+public class InitialsApp {
+    public static void main(String[] args) {
+        System.out.printf("*********  *          *\n" +
+                        "*       *  **        **\n" +
+                        "*       *  * *      * *\n" +
+                        "*********  *  *    *  *\n" +
+                        "*       *  *   *  *   *\n" +
+                        "*       *  *    **    *\n" +
+                        "*********  *    **    *\n"
+
+                );
+    }
+}
---
+++ src/gr/aueb/cf/ch1/SumApp.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.ch1;
+
+public class SumApp {
+    public static void main(String[] args) {
+        int num1=12;
+        int num2=8;
+        int sum12=0;
+        sum12=num1 + num2;
+
+        System.out.println("Το αποτέλεσμα της πρόσθεσης είναι ίσο με " + sum12);
+    }
+}
